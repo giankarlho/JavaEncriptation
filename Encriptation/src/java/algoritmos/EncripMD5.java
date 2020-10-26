56e6d578804b8ccdec21f2192af70191dad3e7cb
Giancarlo Valencia Sotomayor <gvalencia@vallegrande.edu.pe>
2020-10-26 17:48:55 -0500

    Add files via upload

---
+++ Encriptation/src/java/algoritmos/EncripMD5.java
@@ -0,0 +1,23 @@
+package servicios;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncripMD5 {
+    public static String encriptar(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);          
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
